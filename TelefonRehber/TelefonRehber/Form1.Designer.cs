fec117238572348fb00e4ec6b867157ccefa4cbe
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2024-07-30 08:36:09 +0300

    Add files via upload

---
+++ TelefonRehber/TelefonRehber/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace TelefonRehber
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Btn_temizle = new System.Windows.Forms.Button();
+            this.Btn_güncelle = new System.Windows.Forms.Button();
+            this.Btn_sil = new System.Windows.Forms.Button();
+            this.Btn_ekle = new System.Windows.Forms.Button();
+            this.Msk_telefon = new System.Windows.Forms.MaskedTextBox();
+            this.Txt_Mail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Txt_Soyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Txt_Ad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txt_Id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 328);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "REHBER";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(406, 309);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.Btn_temizle);
+            this.groupBox2.Controls.Add(this.Btn_güncelle);
+            this.groupBox2.Controls.Add(this.Btn_sil);
+            this.groupBox2.Controls.Add(this.Btn_ekle);
+            this.groupBox2.Controls.Add(this.Msk_telefon);
+            this.groupBox2.Controls.Add(this.Txt_Mail);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.Txt_Soyad);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.Txt_Ad);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.Txt_Id);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(430, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 329);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "YENİ KİŞİ";
+            // 
+            // Btn_temizle
+            // 
+            this.Btn_temizle.Location = new System.Drawing.Point(113, 292);
+            this.Btn_temizle.Name = "Btn_temizle";
+            this.Btn_temizle.Size = new System.Drawing.Size(75, 23);
+            this.Btn_temizle.TabIndex = 3;
+            this.Btn_temizle.Text = "TEMİZLE";
+            this.Btn_temizle.UseVisualStyleBackColor = true;
+            this.Btn_temizle.Click += new System.EventHandler(this.Btn_temizle_Click);
+            // 
+            // Btn_güncelle
+            // 
+            this.Btn_güncelle.Location = new System.Drawing.Point(31, 292);
+            this.Btn_güncelle.Name = "Btn_güncelle";
+            this.Btn_güncelle.Size = new System.Drawing.Size(75, 23);
+            this.Btn_güncelle.TabIndex = 3;
+            this.Btn_güncelle.Text = "GÜNCELLE";
+            this.Btn_güncelle.UseVisualStyleBackColor = true;
+            this.Btn_güncelle.Click += new System.EventHandler(this.Btn_güncelle_Click);
+            // 
+            // Btn_sil
+            // 
+            this.Btn_sil.Location = new System.Drawing.Point(112, 263);
+            this.Btn_sil.Name = "Btn_sil";
+            this.Btn_sil.Size = new System.Drawing.Size(75, 23);
+            this.Btn_sil.TabIndex = 3;
+            this.Btn_sil.Text = "SİL";
+            this.Btn_sil.UseVisualStyleBackColor = true;
+            this.Btn_sil.Click += new System.EventHandler(this.Btn_sil_Click);
+            // 
+            // Btn_ekle
+            // 
+            this.Btn_ekle.Location = new System.Drawing.Point(31, 263);
+            this.Btn_ekle.Name = "Btn_ekle";
+            this.Btn_ekle.Size = new System.Drawing.Size(75, 23);
+            this.Btn_ekle.TabIndex = 3;
+            this.Btn_ekle.Text = "EKLE";
+            this.Btn_ekle.UseVisualStyleBackColor = true;
+            this.Btn_ekle.Click += new System.EventHandler(this.Btn_ekle_Click);
+            // 
+            // Msk_telefon
+            // 
+            this.Msk_telefon.Location = new System.Drawing.Point(94, 139);
+            this.Msk_telefon.Mask = "(999) 000-0000";
+            this.Msk_telefon.Name = "Msk_telefon";
+            this.Msk_telefon.Size = new System.Drawing.Size(100, 20);
+            this.Msk_telefon.TabIndex = 2;
+            // 
+            // Txt_Mail
+            // 
+            this.Txt_Mail.Location = new System.Drawing.Point(94, 174);
+            this.Txt_Mail.Name = "Txt_Mail";
+            this.Txt_Mail.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Mail.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "TEL:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "MAİL:";
+            // 
+            // Txt_Soyad
+            // 
+            this.Txt_Soyad.Location = new System.Drawing.Point(94, 101);
+            this.Txt_Soyad.Name = "Txt_Soyad";
+            this.Txt_Soyad.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Soyad.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "SOYAD:";
+            // 
+            // Txt_Ad
+            // 
+            this.Txt_Ad.Location = new System.Drawing.Point(94, 61);
+            this.Txt_Ad.Name = "Txt_Ad";
+            this.Txt_Ad.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Ad.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "AD:";
+            // 
+            // Txt_Id
+            // 
+            this.Txt_Id.Location = new System.Drawing.Point(94, 23);
+            this.Txt_Id.Name = "Txt_Id";
+            this.Txt_Id.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Id.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "FOTO EKLE :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(75, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(13, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = ":";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 366);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MaskedTextBox Msk_telefon;
+        private System.Windows.Forms.TextBox Txt_Mail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Txt_Soyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Txt_Ad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txt_Id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_temizle;
+        private System.Windows.Forms.Button Btn_güncelle;
+        private System.Windows.Forms.Button Btn_sil;
+        private System.Windows.Forms.Button Btn_ekle;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TelefonRehber/TelefonRehber/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TelefonRehber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        
+            
+        }
+      SqlConnection bgl = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=rehberapp;Integrated Security=True;Connect Timeout=30;");
+        //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=rehberapp;Integrated Security=True;Connect Timeout=30; 
+       
+        void listele()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From rehbertable",bgl);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        void temizle()
+        {
+            Txt_Ad.Text = "";
+            Txt_Id.Text = "";
+            Txt_Mail.Text = "";
+            Txt_Soyad.Text = "";
+            Txt_Ad.Focus();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+
+   
+        }
+
+        private void Btn_ekle_Click(object sender, EventArgs e)
+        {
+            bgl.Open();
+
+            SqlCommand komut = new SqlCommand("insert into rehbertable (AD,SOYAD,TELEFON,MAİL,PROFİLFOTO) values " +
+                "(@P1,@P2,@P3,@P4,@P5)",bgl);
+            komut.Parameters.AddWithValue("@P1", Txt_Ad.Text);
+            komut.Parameters.AddWithValue("@P2", Txt_Soyad .Text);
+            komut.Parameters.AddWithValue("@P3",Msk_telefon.Text);
+            komut.Parameters.AddWithValue("@P4", Txt_Mail.Text);
+            komut.Parameters.AddWithValue("@P5",label7.Text);
+            komut.ExecuteNonQuery();
+            bgl.Close();
+            MessageBox.Show("Kişi sisteme kaydedildi","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            listele();
+            temizle();
+        }
+
+        private void Btn_temizle_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+
+            Txt_Id.Text = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+           Txt_Ad .Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+           Txt_Soyad .Text = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            Msk_telefon .Text = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+         Txt_Mail  .Text = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            label7.Text=dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+            pictureBox1.ImageLocation = label7.Text;
+        }
+
+        private void Btn_sil_Click(object sender, EventArgs e)
+        {
+            bgl.Open();
+            SqlCommand kmt = new SqlCommand("Delete from rehbertable where ID=" + Txt_Id.Text, bgl);
+            kmt.ExecuteNonQuery();
+            bgl.Close();
+            MessageBox.Show("Kişi rehberden silindi ", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            listele();
+            temizle();
+
+
+        }
+
+        private void Btn_güncelle_Click(object sender, EventArgs e)
+        {
+
+            bgl.Open();
+            SqlCommand kmt = new SqlCommand("update   rehbertable set AD=@p1,SOYAD=@p2,TELEFON=@p3,MAİL=@p4,PROFİLFOTO=@p5 where ID=" + Txt_Id.Text, bgl);
+            kmt.Parameters.AddWithValue("@p1", Txt_Ad.Text);
+            kmt.Parameters.AddWithValue("@p2",Txt_Soyad.Text);
+            kmt.Parameters.AddWithValue("@p3",Msk_telefon.Text);
+            kmt.Parameters.AddWithValue("@p4",Txt_Mail.Text);
+            kmt.Parameters.AddWithValue("@p5", label7.Text);
+            kmt.ExecuteNonQuery();
+            bgl.Close();
+            MessageBox.Show("Kişi rehberden güncellendi ", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+            temizle();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            openFileDialog1.ShowDialog(this);
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+            label7.Text = openFileDialog1.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            openFileDialog1.ShowDialog(this);
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+            label7.Text = openFileDialog1.FileName;
+        }
+    }
+}
